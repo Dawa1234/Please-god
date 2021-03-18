e124c00be93842d45a19d4797fc1aee340c845c5
khangdu <donotdare>
2021-03-18 13:57:53 +0545

    Solutions

---
+++ question/nine.py
@@ -0,0 +1,8 @@
+# Write a program to find a sum of the first n positive integers.
+ #                 sum = (n*(n +1))/2
+
+n = int(input("Enter positive integers:"))
+
+sum = (n*(n +1))/2
+
+print(f"{sum} is the sum of positive integers.")
